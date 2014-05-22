2ca4fc81f9ca51ca359a48bafb30726a6e7a5b83
Purpello <jaysonwebb@Jaysons-MacBook-Air.local>
2014-05-21 19:08:09 -0600

    Completed the home work assignment
    Wrote and tested the code to cache a matrix inverse, using the “cache
    mean” function from the original source as an example.
    
    Replaced the “stub” comments with actual comments describing the code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions are presented below which cache values from resource intensive calculations so that 
+## values that might be needed again can be looked up rather than recomputed.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a matrix object that can be cached using the <<- assignment
+## operator to keep a copy of the matrix in the parent environment.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrx = matrix()) {
+  matrxInv<-NULL                     #set the local value of the matrix inverse to NULL
+  
+  set <- function(mtrx) {          
+    matrx <<- mtrx                   #cache the value of the original matrix
+    matrxInv <<- NULL                #set the cached value of the matrix inverse to NULL
+  }
+  get <- function() matrx
+  setInv <- function(Inv) matrxInv <<- Inv 
+  getInv <- function() matrxInv
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
+## cacheSolve takes the object created by makeCacheMatrix and determines if there already
+## exists a matrix inverse.  If so, it returns the cached value.  If not, it computes a 
+## new value, using solve() to create the inverse.  The assumption is that the inverse exists.
+## The function does not check for that.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrx, ...) {
+  Inv <- matrx$getInv()
+  if(!is.null(Inv)) {
+    message("getting cached matrix inverse")
+    return(Inv)
+  }
+  data <- matrx$get()
+  Inv <- solve(data, ...)
+  matrx$setInv(Inv)
+  Inv
 }
